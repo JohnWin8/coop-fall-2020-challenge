ec483c248fbe6279cf2e024d279eac6b245d18aa
John Winnett <johnwinnett6703@gmail.com>
2020-07-10 14:21:59 -0400

    First Commit Fingers Crossed

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,36 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.list = [self.value]
+        self.undone = []
 
     def add(self, num: int):
-        pass
+        self.list.append(num)
+        self.value += num
+        if len(self.undone) > 0:
+            self.undone.pop()
 
     def subtract(self, num: int):
-        pass
+        self.add(0-num)
 
     def undo(self):
-        pass
-
+        if len(self.list) <= 1:
+            return
+        remove = self.list.pop()
+        self.undone.append(remove)
+        self.value -= remove
+    
     def redo(self):
-        pass
+        if len(self.undone) < 1:
+            return
+        remove = self.undone.pop()
+        self.list.append(remove)
+        self.value += remove
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.redo()
